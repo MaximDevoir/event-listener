cde3b1742fb72553c34cef2c51b99d4bf21f3502
Maxim Devoir <maximdevoir@hotmail.com>
2019-05-10 22:46:00 -0700

    Updated packages and source files
    Additionally, modified config files.

--- src/EventListener.js
+++ src/EventListener.js
@@ -1,6 +1,7 @@
 /**
- * EventListener - A solution to add and remove events from a
- *  target while being compatible with modern browsers, and their use of the
+ * EventListener - A solution to add and remove events from a target while being
+ *  compatible with modern browsers, and their use of the
+ *
  *  .[add|remove]EventListener() methods, as well as older browsers, and their
  *  .[attach|detach]Event() methods.
  * @name EventListener
@@ -8,31 +9,34 @@
 export default class EventListener {
   /**
    * Add an event to target.
+   *
    * @param {*}        target     Target to add event to
    * @param {String}   event      Name of event type to listen for
-   * @param {Function} listener   Listener to execute when event is fired from target
+   * @param {Function} listener   Listener to execute when event is fired from
+   * target
    * @param {Boolean}  useCapture useCapture
    */
   static add(target, event, listener, useCapture = false) {
     if (target.addEventListener) {
-      target.addEventListener(event, listener, useCapture);
+      target.addEventListener(event, listener, useCapture)
     } else if (target.attachEvent) {
-      target.attachEvent(`on${event}`, listener);
+      target.attachEvent(`on${event}`, listener)
     }
   }
 
   /**
    * Remove an event from a target.
+   *
    * @param {*}        target     Target to remove event from
    * @param {String}   event      Name of event type to remove from target
-   * @param {Function} listener   Listener to remove frmo event target
+   * @param {Function} listener   Listener to remove from event target
    * @param {Boolean}  useCapture useCapture
    */
   static remove(target, event, listener, useCapture = false) {
     if (target.removeEventListener) {
-      target.removeEventListener(event, listener, useCapture);
+      target.removeEventListener(event, listener, useCapture)
     } else if (target.detachEvent) {
-      target.detachEvent(`on${event}`, listener);
+      target.detachEvent(`on${event}`, listener)
     }
   }
 }
